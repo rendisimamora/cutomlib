0049ea6fe3a800c485c7f1e6c0924ab7ed99f3f3
rendisimamora <rendisimamora7127@gmail.com>
2020-01-24 18:56:27 +0700

    first commit

---
+++ my-component.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h5>Hello this is my components</h5>
+    <div>
+      <p>My name is {{ name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: 'Mark'
+    }
+  }
+}
+</script>
